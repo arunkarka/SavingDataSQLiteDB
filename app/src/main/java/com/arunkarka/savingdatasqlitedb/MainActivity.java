e11bf747d125d54b6bbe6a04739dcdfeef7c9fd1
arun.karka <arunkarka@gmail.com>
2017-10-18 11:29:59 -0500

    Added click listeners for Creating and deleting DB.

--- app/src/main/java/com/arunkarka/savingdatasqlitedb/MainActivity.java
+++ app/src/main/java/com/arunkarka/savingdatasqlitedb/MainActivity.java
@@ -2,12 +2,50 @@ package com.arunkarka.savingdatasqlitedb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static boolean dbExists = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createDB = (Button) findViewById(R.id.create_db);
+        createDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDatabase();
+            }
+        });
+
+        Button deleteDB = (Button) findViewById(R.id.delete_db);
+        deleteDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteDatabase();
+            }
+        });
+    }
+
+    private void createDatabase() {
+        if (dbExists) {
+            Toast.makeText(this, "Student DB was already created.", Toast.LENGTH_SHORT).show();
+        } else {
+            dbExists = true;
+            Toast.makeText(this, "Student DB created", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void deleteDatabase() {
+        if (dbExists) {
+            dbExists = false;
+            Toast.makeText(this, "Student DB deleted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "DB doesn't exist", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
